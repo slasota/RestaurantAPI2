649c9e2a17e640737b87ac2d09098136db4c69fd
slaso <slaso@DESKTOP-MF5KUJA>
2021-09-08 16:38:30 +0200

    Delete method

--- RestaurantAPI2/Services/RestaurantService.cs
+++ RestaurantAPI2/Services/RestaurantService.cs
@@ -15,6 +15,7 @@ namespace RestaurantAPI2.Services
         int Create(CreateRestaurantDto dto);
         IEnumerable<RestaurantDto> GetAll();
         RestaurantDto GetById(int id);
+        bool Delete(int id);
     }
 
     public class RestaurantService : IRestaurantService
@@ -26,6 +27,20 @@ namespace RestaurantAPI2.Services
         {
             _dbContext = dbContext;
             _mapper = mapper;
+        }
+        public bool Delete(int id)
+        {
+            var restaurant = _dbContext
+                .Restaurants
+                .FirstOrDefault(r => r.Id == id);
+
+            if (restaurant is null)
+                return false;
+
+            _dbContext.Restaurants.Remove(restaurant);
+            _dbContext.SaveChanges();
+            return true;
+
         }
         public RestaurantDto GetById(int id)
         {
@@ -63,5 +78,6 @@ namespace RestaurantAPI2.Services
             _dbContext.SaveChanges();
             return restaurant.Id;
         }
+
     }
 }
