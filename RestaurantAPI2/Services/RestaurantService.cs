68bdafdccadd0b28b4fd5c7dcac399ada90ba016
slaso <slaso@DESKTOP-MF5KUJA>
2021-09-08 19:05:20 +0200

    Add NLoger

--- RestaurantAPI2/Services/RestaurantService.cs
+++ RestaurantAPI2/Services/RestaurantService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RestaurantAPI2.Entities;
 using RestaurantAPI2.Models;
 using System;
@@ -23,11 +24,13 @@ namespace RestaurantAPI2.Services
     {
         private readonly RestaurantDbContext _dbContext;
         private readonly IMapper _mapper;
+        private readonly ILogger<RestaurantService> _logger;
 
-        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper)
+        public RestaurantService(RestaurantDbContext dbContext, IMapper mapper, ILogger<RestaurantService> logger )
         {
             _dbContext = dbContext;
             _mapper = mapper;
+            _logger = logger;
         }
 
         public bool Update(int id, UpdateRestaurantDto dto)
@@ -50,6 +53,8 @@ namespace RestaurantAPI2.Services
         }
         public bool Delete(int id)
         {
+            _logger.LogError($"Restaurant with id: {id} DELETE action invoked");
+
             var restaurant = _dbContext
                 .Restaurants
                 .FirstOrDefault(r => r.Id == id);
