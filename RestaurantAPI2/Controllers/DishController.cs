2545245d076efc04cbf1eeabb3b3ae4b525ca879
slaso <slaso@DESKTOP-MF5KUJA>
2021-09-10 18:21:59 +0200

    CRUD subencji

---
+++ RestaurantAPI2/Controllers/DishController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using RestaurantAPI2.Models;
+using RestaurantAPI2.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI2.Controllers
+{
+    [Route("api/restaurant/{restaurantId}/dish")]
+    [ApiController]
+    public class DishController : ControllerBase
+    {
+        private readonly IDishService _dishService;
+
+        public DishController(IDishService service)
+        {
+            _dishService = service;
+        }
+        [HttpPost]
+        public ActionResult Post([FromRoute] int restaurantId,[FromBody] CreateDishDto dto)
+        {
+            var newDishId = _dishService.Create(restaurantId, dto);
+            return Created($"/api/restaurant/{restaurantId}/dish/{newDishId}", null);
+        }
+        [HttpGet("{dishId}")]
+        public ActionResult<DishDto> Get([FromRoute] int restaurantId, [FromRoute] int dishId )
+        {
+            DishDto dish = _dishService.GetById(restaurantId, dishId);
+            
+            return Ok(dish);
+        }
+
+        [HttpGet]
+        public ActionResult<List<DishDto>> GetAll([FromRoute] int restaurantId)
+        {
+            var result = _dishService.GetAll(restaurantId);
+
+            return Ok(result);
+
+        }
+        [HttpDelete]
+        public ActionResult DeleteAll([FromRoute] int restaurantId)
+        {
+            _dishService.RemoveAll(restaurantId);
+            return NoContent();
+        }
+        [HttpDelete("{dishId}")]
+        public ActionResult Delete([FromRoute] int restaurantId, [FromRoute] int dishId)
+        {
+            _dishService.Remove(restaurantId, dishId);
+            return NoContent();
+        }
+    }
+}
--- RestaurantAPI2/RestaurantMappingProfile.cs
+++ RestaurantAPI2/RestaurantMappingProfile.cs
@@ -22,6 +22,8 @@ namespace RestaurantAPI2
 
             CreateMap<CreateRestaurantDto, Restaurant>()
                 .ForMember(r => r.Address, c => c.MapFrom(dto => new Address() { City = dto.City, Street = dto.Street, PostalCode = dto.PostalCode }));
+
+            CreateMap<CreateDishDto, Dish>();
         }
     }
 }
